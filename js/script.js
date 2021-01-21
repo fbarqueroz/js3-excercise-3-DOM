3d65f53ec55150b878e9737ba80b0134b09667eb
fbarqueroz <65320282+fbarqueroz@users.noreply.github.com>
2021-01-21 17:11:54 -0600

    My first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/**
+ * Ejercicio #3:
+ * Usando querySelector y querySelectorAll
+ *
+ * ¿Que voy a evaluar?
+ * 1. Que funcione
+ * 2. Buenas practicas
+ *
+ * NOTA: no se puede modificar el codigo HTML de la pagina
+ */
+
+/*
+  EJERCICIO A: --------------------------------------------
+  
+  Selecciona y console.log el <h3> que dice "Esto
+  es una lista"
+*/
+
+// Codigo ejercicio 1
+
+const excerciseOne = document.querySelector('h3');
+console.log(excerciseOne.innerText);
+
+/*
+  EJERCICIO B: --------------------------------------------
+  
+  Selecciona y loguea el primer y el ultimo elemento
+  de la lista
+*/
+
+/*
+  EJERCICIO C: --------------------------------------------
+  
+  Selecciona y loguea el input de tipo "email" del
+  formulario
+*/
+
+/*
+  EJERCICIO D: --------------------------------------------
+  
+  Selecciona y loguea la fila de la tabla que tiene "JavaScript"
+*/
+
+/*
+  EJERCICIO E: --------------------------------------------
+  
+  Selecciona y loguea la celda que dice CSS
+*/
+
+/*
+  EJERCICIO F: --------------------------------------------
+  
+  Investiga un metodo de algun elemento y trae un ejemplo de
+  para que lo usarias
+*/
